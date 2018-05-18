1a05daf3d4f6744600a238a82a854d0bcede46b5
Yeontae Kim <ytk007@gmail.com>
2018-05-18 15:47:39 -0700

    Add tests for GET /messages

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,18 @@ const request = require('supertest');
 const {app} = require('../server');
 const {Message} = require('../models/message');
 
+const messages = [{
+    text: 'First test message',
+    isSender: true
+}, {
+    text: 'Second test message',
+    isSender: false
+}]
+
 beforeEach((done) => {
-    Message.remove({}).then(() => done());
+    Message.remove({}).then(() => {
+        return Message.insertMany(messages);
+    }).then(() => done());
 });
 
 describe('Server', () => {
@@ -37,12 +47,41 @@ describe('Server', () => {
                         return done(err);
                     }
 
-                    Message.find().then((messages) => {
+                    Message.find({text, isSender}).then((messages) => {
                         expect(messages.length).toBe(1);
                         expect(messages[0].text).toBe(text);
                         done();
                     }).catch((e) => done(e));
                 });
+        });
+
+        it('should not create a message with invalid data', (done) => {
+            request(app)
+                .post('/messages')
+                .send({})
+                .expect(400)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    Message.find().then((messages) => {
+                        expect(messages.length).toBe(2);
+                        done();
+                    }).catch((e) => done(e));
+                });
+        });
+    });
+
+    describe('GET /messages', () => {
+        it('should get all messages', (done) => {
+            request(app)
+                .get('/messages')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.messages.length).toBe(2);
+                })
+                .end(done);
         });
     });
 });
