a873d7d9227e33bb33d6bf9c1135bfeffcbdb292
Yeontae Kim <ytk007@gmail.com>
2018-05-18 22:54:22 -0700

    Addd DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -108,6 +108,14 @@ app.post('/action', (req, res) => {
     res.send('Asked to get weather data');
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server started on ${port}...`);
 });
